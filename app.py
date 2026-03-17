5ec1dbce185a75778e1e8d636f140efcd3012b15
xrhgaming94-lab <xrhgaming94@gmail.com>
2026-03-18 05:17:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import urllib3
 import random
 
 # Configuration
-TOKEN_BATCH_SIZE = 326
+TOKEN_BATCH_SIZE = 660
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Global State for Batch Management
